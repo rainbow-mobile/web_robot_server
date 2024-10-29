0ec817dc567e5dae9bb3f6bb3569ec61f3edc725
yujinHeo_nuc <yjheo4@rainbow-robotics.com>
2024-10-29 14:56:02 +0900

    network

--- routers/setting/network_router.js
+++ routers/setting/network_router.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const router = express.Router();
 const fs = require("fs");
 const network = require("../../src/network");
+const logger = require("../../src/log/logger");
 
 router.use(bodyParser.json());
 router.use(cors());
@@ -60,7 +61,7 @@ router.get("/network/wifi/scan", (req, res) => {
   }
 });
 
-router.put("/network/ethernet", (req, res) => {
+router.put("/network/:device", (req, res) => {
   try {
     if (
       req.body.name == undefined ||
@@ -95,53 +96,7 @@ router.put("/network/ethernet", (req, res) => {
       return;
     }
 
-    network
-      .setIP(req.body)
-      .then((data) => {
-        res.send(req.body);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
-  } catch (e) {}
-});
-
-router.put("/network/wifi", (req, res) => {
-  try {
-    if (
-      req.body.name == undefined ||
-      req.body.name == null ||
-      req.body.name == ""
-    ) {
-      res.sendStatus(400);
-      return;
-    }
-    if (req.body.ip == undefined || req.body.ip == null || req.body.ip == "") {
-      res.sendStatus(400);
-      return;
-    }
-    if (
-      req.body.gateway == undefined ||
-      req.body.gateway == null ||
-      req.body.gateway == ""
-    ) {
-      res.sendStatus(400);
-      return;
-    }
-    if (
-      req.body.subnet == undefined ||
-      req.body.subnet == null ||
-      req.body.subnet == ""
-    ) {
-      res.sendStatus(400);
-      return;
-    }
-    if (req.body.dns == undefined || req.body.dns == null) {
-      res.sendStatus(400);
-      return;
-    }
-
+    logger.info("set IP " + req.params.device + " : ", req.body);
     network
       .setIP(req.body)
       .then((data) => {
@@ -152,6 +107,7 @@ router.put("/network/wifi", (req, res) => {
         res.sendStatus(500);
       });
   } catch (e) {
+    console.error(e);
     res.sendStatus(500);
   }
 });
