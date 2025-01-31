9a8d84665ab1a3e60aab9ca7b82b156199116803
yujinHeo <yjheo4@rainbow-robotics.com>
2025-01-31 10:37:10 +0900

    fix:frs robots-init yet

--- src/modules/sockets/kafka/kafka.service.ts
+++ src/modules/sockets/kafka/kafka.service.ts
@@ -11,26 +11,31 @@ export class KafkaClientService {
   private client:Consumer = null;
 
   async connect(){
-    global.kafka_url = "10.108.1.180:9092"
+    global.kafka_url = "192.168.1.195:9092"
     socketLogger.info(`[KAFKA] Try to connect KAFKA : ${global.kafka_url}`)
     this.kafka = new Kafka({
         brokers:[global.kafka_url],
+
     })
     this.client = this.kafka.consumer({groupId:'frs-kafka'});
 
-    await this.client.connect();
-    await this.client.subscribe({topic:'kafka-emit1', fromBeginning:true})
-
-    await this.client.run({
-    eachMessage: async ({ topic, partition, message }) => {
-        console.log({
-            topic: topic.toString(),
-            partition: partition.toString(),
-            key: message.key.toString(),
-            value: message.value.toString(),
-            header: message.headers.toString()
-        });
-    },
-  });
+    try{
+      await this.client.connect();
+      await this.client.subscribe({topic:'kafka-emit1', fromBeginning:true})
+  
+      await this.client.run({
+      eachMessage: async ({ topic, partition, message }) => {
+          console.log({
+              topic: topic.toString(),
+              partition: partition.toString(),
+              key: message.key.toString(),
+              value: message.value.toString(),
+              header: message.headers.toString()
+          });
+        }
+      });
+    }catch(error){
+      socketLogger.error(`[KAFKA] ${error.name}: ${error.message}`);
+    }
   }
 }
