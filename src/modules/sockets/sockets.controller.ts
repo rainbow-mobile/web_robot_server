7b454c26add8f4b1c1a2448314ab10e66797bce5
dfd1123 <dfd1123@naver.com>
2025-05-15 09:08:51 +0900

    fix: lint 에러 수정

--- src/modules/sockets/sockets.controller.ts
+++ src/modules/sockets/sockets.controller.ts
@@ -219,7 +219,7 @@ export class SocketsController implements OnModuleInit {
   })
   async lidarOn(@Body() data: EmitOnOffDto, @Res() res: Response) {
     try {
-      if(this.socketGateway.slamnav){
+      if (this.socketGateway.slamnav) {
         httpLogger.info(
           `[SOCKET] lidar OnOff: ${data.command} -> ${data.frequency}`,
         );
@@ -228,8 +228,11 @@ export class SocketsController implements OnModuleInit {
           JSON.stringify({ ...data, time: Date.now().toString() }),
         );
         res.send();
-      }else{
-        throw new HttpException('SLAMNAV가 연결되지 않았습니다.',HttpStatus.BAD_GATEWAY);
+      } else {
+        throw new HttpException(
+          'SLAMNAV가 연결되지 않았습니다.',
+          HttpStatus.BAD_GATEWAY,
+        );
       }
     } catch (error) {
       httpLogger.error(`[SOCKET] lidar OnOff: ${errorToJson(error)}`);
@@ -243,7 +246,7 @@ export class SocketsController implements OnModuleInit {
   })
   async pathOn(@Body() data: EmitOnOffDto, @Res() res: Response) {
     try {
-      if(this.socketGateway.slamnav){
+      if (this.socketGateway.slamnav) {
         httpLogger.info(
           `[SOCKET] path OnOff: ${data.command} -> ${data.frequency}`,
         );
@@ -252,8 +255,11 @@ export class SocketsController implements OnModuleInit {
           JSON.stringify({ ...data, time: Date.now().toString() }),
         );
         res.send();
-      }else{
-        throw new HttpException('SLAMNAV가 연결되지 않았습니다.',HttpStatus.BAD_GATEWAY);
+      } else {
+        throw new HttpException(
+          'SLAMNAV가 연결되지 않았습니다.',
+          HttpStatus.BAD_GATEWAY,
+        );
       }
     } catch (error) {
       httpLogger.error(`[SOCKET] path OnOff: ${errorToJson(error)}`);
