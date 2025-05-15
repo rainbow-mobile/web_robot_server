d5500dc0c0cdd116e3dad47054f818dd6389d698
dfd1123 <dfd1123@naver.com>
2025-05-15 21:48:56 +0900

    chore: dist 폴더 Git 추적 제거

--- dist/modules/apis/upload/upload.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class UploadService {
-    private storage;
-    private upload;
-    downloadMap(fileName: string): Promise<unknown>;
-    zipFolder(sourceFolderPath: string, zipFilePath: string): Promise<unknown>;
-    unzipFolder(zipFilePath: string, extractToPath: string): Promise<unknown>;
-}
