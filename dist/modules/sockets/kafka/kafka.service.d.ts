eb49b23aba110879f3bd6a9d8a8d10556f20a222
dfd1123 <dfd1123@naver.com>
2025-06-30 14:04:59 +0900

    chore: dist 폴더 Git 추적 제거

--- dist/modules/sockets/kafka/kafka.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { UploadService } from 'src/modules/apis/upload/upload.service';
-export declare class KafkaClientService {
-    private readonly uploadService;
-    private kafka;
-    private client;
-    constructor(uploadService: UploadService);
-    connect(): Promise<void>;
-    mapPublish(data: any): Promise<void>;
-}
