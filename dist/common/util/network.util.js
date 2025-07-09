0c7c563a79652096d41af751ca1f42c3ca954213
dfd1123 <dfd1123@naver.com>
2025-07-09 20:33:14 +0900

    chore: Release rrs-20250709203310

---
+++ dist/common/util/network.util.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getMacAddresses = getMacAddresses;
+exports.stringifyAllValues = stringifyAllValues;
+const os = require("os");
+function getMacAddresses() {
+    const networkInterfaces = os.networkInterfaces();
+    const macAddresses = [];
+    for (const [interfaceName, interfaces] of Object.entries(networkInterfaces)) {
+        interfaces.forEach((iface) => {
+            if (!iface.internal && iface.mac) {
+                macAddresses.push({
+                    interface: interfaceName,
+                    mac: iface.mac,
+                });
+            }
+        });
+    }
+    return macAddresses;
+}
+function stringifyAllValues(obj) {
+    for (const key in obj) {
+        if (typeof obj[key] === 'object') {
+            stringifyAllValues(obj[key]);
+        }
+        else {
+            obj[key] = String(obj[key]);
+        }
+    }
+    return obj;
+}
+module.exports = {
+    getMacAddresses: getMacAddresses,
+    stringifyAllValues: stringifyAllValues,
+};
+//# sourceMappingURL=network.util.js.map
