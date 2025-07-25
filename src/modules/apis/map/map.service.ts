c576ae7de8c64948235a0c981040dae83d1247e4
dfd1123 <dfd1123@naver.com>
2025-07-25 13:40:52 +0900

    feat: 맵 삭제 기능 추가 및 서비스 로직 구현

--- src/modules/apis/map/map.service.ts
+++ src/modules/apis/map/map.service.ts
@@ -1,5 +1,5 @@
 import httpLogger from '@common/logger/http.logger';
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { SocketGateway } from '@sockets/gateway/sockets.gateway';
 import * as fs from 'fs';
 import { homedir } from 'os';
@@ -133,6 +133,30 @@ export class MapService {
         reject({
           status: HttpStatus.GATEWAY_TIMEOUT,
           data: { message: '프로그램이 연결되지 않았습니다' },
+        });
+      }
+    });
+  }
+
+  deleteMap(mapNm: string) {
+    const mapDir = Path.join(this.mapDir, mapNm);
+
+    return new Promise((resolve, reject) => {
+      if (fs.existsSync(mapDir)) {
+        fs.rm(mapDir, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({
+              status: HttpStatus.OK,
+              data: { message: 'Map deleted successfully' },
+            });
+          }
+        });
+      } else {
+        reject({
+          status: HttpStatus.NOT_FOUND,
+          data: { message: 'Map not found' },
         });
       }
     });
