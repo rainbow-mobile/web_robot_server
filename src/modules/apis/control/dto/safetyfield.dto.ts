9a400f25926cc69089bca5a90ca9b491398354f4
yujinHeo <yjheo4@rainbow-robotics.com>
2025-07-29 11:16:41 +0900

    fix: SetSafetyFieldDto type error

--- src/modules/apis/control/dto/safetyfield.dto.ts
+++ src/modules/apis/control/dto/safetyfield.dto.ts
@@ -1,11 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber } from 'class-validator';
 
 export class SetSafetyFieldDto {
-    @IsNumber()
-    @ApiProperty({
-        description:'',
-        example: 0
-    })
-    field: number;
+  @IsNumber()
+  @Type(()=>Number)
+  @ApiProperty({
+    description: '',
+    example: 0,
+  })
+  field: number;
 }
