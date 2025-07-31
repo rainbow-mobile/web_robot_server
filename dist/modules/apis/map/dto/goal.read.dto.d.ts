886e2f45ea0b88c3e13dced7e636dcd2d7918e61
yujinHeo <yjheo4@rainbow-robotics.com>
2025-07-31 16:26:48 +0900

    chore: dist 폴더 Git 추적 제거

--- dist/modules/apis/map/dto/goal.read.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { PaginationRequest } from '@common/pagination/pagination.request';
-export declare class GoalReadDto extends PaginationRequest {
-    type?: string;
-    searchText?: string;
-    sortOption?: string;
-}
--- dist/modules/config/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: () => {
-    dataBasePath: string;
-};
-export default _default;
