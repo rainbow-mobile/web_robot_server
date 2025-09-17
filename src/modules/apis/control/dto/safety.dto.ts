3c026f024d4cb20d752e8deb10ed7e599e1bbd24
yujinHeo <yjheo4@rainbow-robotics.com>
2025-09-17 09:23:32 +0900

    feat: safetyrequest

---
+++ src/modules/apis/control/dto/safety.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
+
+export class SetSafetyFieldDto {
+  @IsNumber()
+  @Type(() => Number)
+  @ApiProperty({
+    description: '',
+    example: 0,
+  })
+  field: number;
+}
+
+export class ResetSafetyFlagDto {
+    @IsString()
+    @ApiProperty({
+      description: '리셋할 플래그 이름을 입력하세요.',
+      example: 'bumper',
+      enum: ['bumper', 'interlock', 'obstacle', 'operationStop'],
+    })
+    reset_flag: string;
+  }
+  
